de27176febb27b9bed2ce256462913219d69bb9d
madeval <alvarez.fing@gmail.com>
2022-07-27 15:53:16 -0300

    feat: example

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import { useEffect, useState } from 'react'
+
 function App() {
-  return (
-    <div className="App">
-      <img src='vite.svg' />
-    </div>
-  )
+  const [number, setNumber] = useState(0)
+
+  useEffect(() => {
+    const intervalID = setInterval(() => {
+      console.log('setNumber(1)')
+      setNumber(lastNumber => lastNumber + 1)
+    }, 10000)
+
+    return () => {
+      console.log('return of useeffect')
+      clearInterval(intervalID)
+    }
+  }, [number])
+
+  return <div className="App">Contador: {number}</div>
 }
 
 export default App
